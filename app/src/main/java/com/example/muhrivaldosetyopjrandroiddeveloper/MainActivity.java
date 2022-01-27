b61b83e958fcbebe2c5e796030043e4d82bbe69c
Rivaldz <rivalzsetyo@gmail.com>
2022-01-27 18:51:21 +0700

    create login view

--- app/src/main/java/com/example/muhrivaldosetyopjrandroiddeveloper/MainActivity.java
+++ app/src/main/java/com/example/muhrivaldosetyopjrandroiddeveloper/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.muhrivaldosetyopjrandroiddeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button submit;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit = findViewById(R.id.button);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+
+
     }
 }
