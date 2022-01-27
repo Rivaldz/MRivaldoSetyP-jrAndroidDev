ade2aedfbc54336553dd31d6a1b0de6dda8194e1
Rivaldz <rivalzsetyo@gmail.com>
2022-01-28 00:36:46 +0700

    setup layout add agent

---
+++ app/src/main/java/com/example/muhrivaldosetyopjrandroiddeveloper/AddAgent.java
@@ -0,0 +1,14 @@
+package com.example.muhrivaldosetyopjrandroiddeveloper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAgent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_agent);
+    }
+}
--- app/src/main/java/com/example/muhrivaldosetyopjrandroiddeveloper/HomeActivity.java
+++ app/src/main/java/com/example/muhrivaldosetyopjrandroiddeveloper/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.muhrivaldosetyopjrandroiddeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void agenListClick(View view) {
+        startActivity(new Intent(HomeActivity.this, AgenList.class));
+    }
+
+    public void addAgenClick(View view) {
+        startActivity(new Intent(HomeActivity.this, AddAgent.class));
     }
 }
